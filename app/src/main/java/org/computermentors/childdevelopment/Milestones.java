6ced34d1b6ff3f9a9bb81df5bb737695e56e6084
N. Dukes <nyjel.dukes@gmail.com>
2020-02-08 21:56:35 -0500

    Placed place holder information.

---
+++ app/src/main/java/org/computermentors/childdevelopment/Milestones.java
@@ -0,0 +1,8 @@
+package org.computermentors.childdevelopment;
+
+public class Milestones {
+    public static String [] headers = new String[]{"Age 0 - 3 months", "Age 3 - 6 months", "Age 6 - 9 months", "Age 9 - 12 months", "Age 12 - 18 Months", "Age 18 - 24 months", "Age 2 - 3 years", "Age 3 - 4 years", "Age 4 - 5 years"};
+    public static int[] resourceIds = new int[]{};
+    public static String[] positive = new String[]{"Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6", "Page 7", "Page 8", "Page 9",};
+    public static String[] concerns = new String[]{"Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6", "Page 7", "Page 8", "Page 9",};
+}
