f5cb9deccf693cfca763fa5e4ae939f96a3f7b15
N. Dukes <nyjel.dukes@gmail.com>
2020-02-08 22:11:21 -0500

    Added ListFragment class

---
+++ app/src/main/java/org/computermentors/childdevelopment/ListFragments.java
@@ -0,0 +1,19 @@
+package org.computermentors.childdevelopment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ListFragments extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container);
+        return view;
+    }
+}
