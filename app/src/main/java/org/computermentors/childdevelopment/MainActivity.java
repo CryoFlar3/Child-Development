4b2b56157e5cdef0af2f387fcd36f72481d9b22a
N. Dukes <nyjel.dukes@gmail.com>
2020-02-08 22:35:16 -0500

    Fragment foundation laid

--- app/src/main/java/org/computermentors/childdevelopment/MainActivity.java
+++ app/src/main/java/org/computermentors/childdevelopment/MainActivity.java
@@ -1,6 +1,8 @@
 package org.computermentors.childdevelopment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListFragment fragment = new ListFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeHolder, fragment);
+        fragmentTransaction.commit();
     }
 }
