bc860131ccaf1b59cc1fa87a486197456852fdd7
N. Dukes <nyjel.dukes@gmail.com>
2020-02-09 22:11:53 -0500

    Set up list to display

--- app/src/main/java/org/computermentors/childdevelopment/ListFragment.java
+++ app/src/main/java/org/computermentors/childdevelopment/ListFragment.java
@@ -8,12 +8,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
         return view;
     }
 }
