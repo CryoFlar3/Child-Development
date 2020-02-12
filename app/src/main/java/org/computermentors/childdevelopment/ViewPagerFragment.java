fa13dd57eb792cd495abe4c1667a95a63af95b02
Nyjel <nyjel.dukes@gmail.com>
2020-02-12 11:49:08 -0500

    Can flip between different fragments now

--- app/src/main/java/org/computermentors/childdevelopment/ViewPagerFragment.java
+++ app/src/main/java/org/computermentors/childdevelopment/ViewPagerFragment.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class ViewPagerFragment extends Fragment {
 
@@ -20,6 +21,25 @@ public class ViewPagerFragment extends Fragment {
         int index= getArguments().getInt(KEY_AGE_INDEX);
         getActivity().setTitle(Milestones.headers[index]);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        final PositiveFragment positiveFragment = new PositiveFragment();
+        final CautionFragment cautionFragment = new CautionFragment();
+
+
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return position == 0 ? positiveFragment : cautionFragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
         return view;
     }
 
