a550a6fddc95d7866f9fe633289fc4b70c54546e
N. Dukes <nyjel.dukes@gmail.com>
2020-02-11 22:10:40 -0500

    App ready for milestones

--- app/src/main/java/org/computermentors/childdevelopment/MainActivity.java
+++ app/src/main/java/org/computermentors/childdevelopment/MainActivity.java
@@ -9,29 +9,33 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnAgeSelectedInterface{
 
+    public static final String LIST_FRAGMENT = "list_fragment";
+    public static final String VIEWPAGER_FRAGMENT = "viewpager_fragment";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
+        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentByTag(LIST_FRAGMENT);
         if (savedFragment == null) {
             ListFragment fragment = new ListFragment();
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.placeHolder, fragment);
+            fragmentTransaction.add(R.id.placeHolder, fragment, LIST_FRAGMENT);
             fragmentTransaction.commit();
         }
     }
 
     @Override
     public void onListAgeSelected(int index) {
-        Toast.makeText(this, Milestones.headers[index], Toast.LENGTH_SHORT).show();
-
         ViewPagerFragment fragment = new ViewPagerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ViewPagerFragment.KEY_AGE_INDEX, index);
+        fragment.setArguments(bundle);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.placeHolder, fragment);
+        fragmentTransaction.replace(R.id.placeHolder, fragment, VIEWPAGER_FRAGMENT);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
--- app/src/main/java/org/computermentors/childdevelopment/ViewPagerFragment.java
+++ app/src/main/java/org/computermentors/childdevelopment/ViewPagerFragment.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ViewPagerFragment extends Fragment {
+
+    public static final String KEY_AGE_INDEX = "age_index";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        int index= getArguments().getInt(KEY_AGE_INDEX);
+        getActivity().setTitle(Milestones.headers[index]);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
     }
 }
