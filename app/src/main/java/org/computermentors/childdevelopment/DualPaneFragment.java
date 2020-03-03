4152101d05451bc717b97327c91a4e18c6544e09
Nyjel <nyjel.dukes@gmail.com>
2020-03-03 18:53:58 -0500

    Added DualPaneFragment Code

--- app/src/main/java/org/computermentors/childdevelopment/DualPaneFragment.java
+++ app/src/main/java/org/computermentors/childdevelopment/DualPaneFragment.java
@@ -8,11 +8,41 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class DualPaneFragment extends Fragment {
+    private static final String POSITIVE_FRAGMENT = "positive_fragment";
+    private static final String CONCERNS_FRAGMENT = "concerns_fragment";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        int index= getArguments().getInt(ViewPagerFragment.KEY_AGE_INDEX);
+        getActivity().setTitle(Milestones.headers[index]);
+        View view = inflater.inflate(R.layout.fragment_dualpane, container, false);
+
+        FragmentManager fragmentManager = getChildFragmentManager();
+
+        PositiveFragment savedPositiveFragment = (PositiveFragment) fragmentManager.findFragmentByTag(POSITIVE_FRAGMENT);
+
+        if (savedPositiveFragment == null){
+            PositiveFragment positiveFragment = new PositiveFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt(ViewPagerFragment.KEY_AGE_INDEX, index);
+            positiveFragment.setArguments(bundle);
+            fragmentManager.beginTransaction().add(R.id.leftPlaceholder, positiveFragment, POSITIVE_FRAGMENT).commit();
+        }
+
+        ConcernsFragment savedConcernsFragment = (ConcernsFragment) fragmentManager.findFragmentByTag(CONCERNS_FRAGMENT);
+
+        if (savedConcernsFragment == null){
+            ConcernsFragment concernsFragment = new ConcernsFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt(ViewPagerFragment.KEY_AGE_INDEX, index);
+            concernsFragment.setArguments(bundle);
+            fragmentManager.beginTransaction().add(R.id.rightPlaceholder, concernsFragment, CONCERNS_FRAGMENT).commit();
+        }
+
+        return view;
     }
 }
